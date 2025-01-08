02eda37b33597f6cf12e81e9ce5a4fd505508732
purpleb3ar <goro.takemura@gmail.com>
2025-01-08 02:32:32 +0100

    changed healtchecks to conform to kubernetes conventions

--- auth/src/modules/health/health.controller.ts
+++ auth/src/modules/health/health.controller.ts
@@ -1,9 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
-import { HealthCheckService, MongooseHealthIndicator } from '@nestjs/terminus';
+import { Controller, Get, HttpCode } from '@nestjs/common';
+import {
+  HealthCheck,
+  HealthCheckService,
+  MongooseHealthIndicator,
+} from '@nestjs/terminus';
 import { NatsHealthIndicator } from './indicator/nats.health';
 import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 
-@Controller('health')
+@Controller()
 export class HealthController {
   constructor(
     private health: HealthCheckService,
@@ -12,9 +16,9 @@ export class HealthController {
   ) {}
 
   @ApiOperation({
-    summary: 'Checks the health of the service',
+    summary: 'Checks the readiness of the service',
     description:
-      'This route performs various health checks to determine whether the service is healthy and ready to handle requests.\n\nThe following checks are performed: \n\nWhether the `mongodb` instance is accessible.\n\nWhether the `nats` instance can receive events.',
+      'This route performs various health checks to determine whether the service is ready and ready to handle requests.\n\nThe following checks are performed: \n\nWhether the `mongodb` instance is accessible.\n\nWhether the `nats` instance can receive events.',
   })
   @ApiResponse({
     status: 200,
@@ -24,11 +28,27 @@ export class HealthController {
     status: 503,
     description: 'The service is not healthly and unable to handle requests',
   })
-  @Get()
+  @Get('/readyz')
+  @HealthCheck()
   check() {
     return this.health.check([
       () => this.mongoose.pingCheck('mongodb', { timeout: 1500 }),
       () => this.nats.pingCheck('nats'),
     ]);
+  }
+
+  @ApiOperation({
+    summary: 'Checks the liveness of the service',
+    description:
+      'This route is called by `kubernetes` liveness probe to determine whether the container is live. Simply being able to response is a sign of liveness and as such no other healtchecks are performed.',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'The service is live.',
+  })
+  @Get('/livez')
+  @HttpCode(200)
+  checkLiveness() {
+    return this.health.check([]);
   }
 }
